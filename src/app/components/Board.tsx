8678d6135dd039cd07d278b071ac7f4a64cf7a18
RickKollenbroich93 <rick.kollenbroich93@googlemail.com>
2021-08-17 22:25:10 +0200

    Fix issue with button

--- src/app/components/Board.tsx
+++ src/app/components/Board.tsx
@@ -35,20 +35,22 @@ function Board(): JSX.Element {
             Reset Game!
           </button>
         </div>
-        <div className="board-row">
-          {renderSquare(0)}
-          {renderSquare(1)}
-          {renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {renderSquare(3)}
-          {renderSquare(4)}
-          {renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {renderSquare(6)}
-          {renderSquare(7)}
-          {renderSquare(8)}
+        <div className="boardwraper">
+          <div className="board-row">
+            {renderSquare(0)}
+            {renderSquare(1)}
+            {renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {renderSquare(3)}
+            {renderSquare(4)}
+            {renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {renderSquare(6)}
+            {renderSquare(7)}
+            {renderSquare(8)}
+          </div>
         </div>
       </div>
     );
