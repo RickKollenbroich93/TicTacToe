41e97a42d286f3009a46546c0321f02c4e2b4afc
RickKollenbroich93 <rick.kollenbroich93@googlemail.com>
2021-08-17 21:44:43 +0200

    Add game functions

--- src/app/components/Square.tsx
+++ src/app/components/Square.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
+// import styles from './src/App.module.css';
 
-function Square(): JSX.Element {
-  return <button className="square">{/* TODO */}</button>;
+function Square(props: {
+  onClick: () => void;
+  value:
+    | boolean
+    | React.ReactChild
+    | React.ReactFragment
+    | React.ReactPortal
+    | null
+    | undefined;
+}): JSX.Element {
+  if (props.value === 'X') {
+    return (
+      <button className="square" onClick={() => props.onClick()}>
+        <span className="X">{props.value}</span>
+      </button>
+    );
+  } else {
+    return (
+      <button className="square" onClick={() => props.onClick()}>
+        <span className="O">{props.value}</span>
+      </button>
+    );
+  }
 }
 
 export default Square;
